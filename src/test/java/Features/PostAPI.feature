58712b8ba072813d9b94a40c38c570399bc3b277
05-Prajwal <prajwaldivakar3@gmail.com.com>
2024-07-21 13:51:28 +0530

    SchemaValidation

--- src/test/java/Features/PostAPI.feature
+++ src/test/java/Features/PostAPI.feature
@@ -6,10 +6,17 @@ Feature: Create New Users
       | firstName | lastName | subjectId | id  |
       | Iphone    | IOS      | 12        | 143 |
     Then Validate the response code 201
+    And Validate the response with JsonSchema of users
 
-  Scenario: Create a new Subject
+
+  Scenario Outline: Create a new Subject
     Given I Triggered the URL
     When I want the to create new subjects
-      | id | name |
-      | 3  | API  |
+      | id   | name   |
+      | <id> | <name> |
     Then Validate the response code 201
+    And Validate the response with JsonSchema of subjects
+    Examples:
+      | id | name |
+      | 3  | data |
+      | 4  | AI   |
--- src/test/java/StepDefinations/API_Triggering.java
+++ src/test/java/StepDefinations/API_Triggering.java
@@ -1,12 +1,15 @@
 package StepDefinations;
 
 
+import io.cucumber.core.internal.com.fasterxml.jackson.databind.jsonschema.JsonSchema;
 import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -15,6 +18,7 @@ import org.example.Configure;
 import org.json.simple.JSONObject;
 import org.junit.Assert;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +31,7 @@ public class API_Triggering {
     public int Status_code;
     public RequestSpecification Request;
     public Response Response;
-    public List<Map<String,String>> data;
+    public List<Map<String, String>> data;
 
     @Given("I Triggered the URL")
     public void I_Triggered_the_URL() {
@@ -55,7 +59,7 @@ public class API_Triggering {
     }
 
     @Then("Validate the users list")
-    public void Validate_the_users_list(DataTable table){
+    public void Validate_the_users_list(DataTable table) {
         try {
             Status_code = Response.getStatusCode();
             String Response_body = Response.getBody().asString();
@@ -63,13 +67,13 @@ public class API_Triggering {
 
             JsonPath jsnp = Response.jsonPath();
 
-            data= table.asMaps();
+            data = table.asMaps();
             System.out.println(data.get(1).get("id"));
 
-            Assert.assertEquals(data.get(0).get("id"),jsnp.get("id[0]").toString());
-            Assert.assertEquals(data.get(1).get("id"),jsnp.get("id[1]").toString());
-            Assert.assertEquals(data.get(0).get("name"),jsnp.get("firstName[0]"));
-            Assert.assertEquals(data.get(1).get("name"),jsnp.get("firstName[1]"));
+            Assert.assertEquals(data.get(0).get("id"), jsnp.get("id[0]").toString());
+            Assert.assertEquals(data.get(1).get("id"), jsnp.get("id[1]").toString());
+            Assert.assertEquals(data.get(0).get("name"), jsnp.get("firstName[0]"));
+            Assert.assertEquals(data.get(1).get("name"), jsnp.get("firstName[1]"));
 
         } catch (Exception e) {
             System.out.println("Exception: " + e);
@@ -79,7 +83,7 @@ public class API_Triggering {
     }
 
     @Then("Validate the subjects list")
-    public void Validate_the_subjects_list(DataTable table){
+    public void Validate_the_subjects_list(DataTable table) {
         try {
             Status_code = Response.getStatusCode();
             String Response_body = Response.getBody().asString();
@@ -87,13 +91,13 @@ public class API_Triggering {
 
             JsonPath jsnp = Response.jsonPath();
 
-            List<Map<String,String>> data= table.asMaps();
+            List<Map<String, String>> data = table.asMaps();
             System.out.println(data.get(1).get("id"));
 
-            Assert.assertEquals(data.get(0).get("id"),jsnp.get("id[0]").toString());
-            Assert.assertEquals(data.get(1).get("id"),jsnp.get("id[1]").toString());
-            Assert.assertEquals(data.get(0).get("Subject"),jsnp.get("name[0]"));
-            Assert.assertEquals(data.get(1).get("Subject"),jsnp.get("name[1]"));
+            Assert.assertEquals(data.get(0).get("id"), jsnp.get("id[0]").toString());
+            Assert.assertEquals(data.get(1).get("id"), jsnp.get("id[1]").toString());
+            Assert.assertEquals(data.get(0).get("Subject"), jsnp.get("name[0]"));
+            Assert.assertEquals(data.get(1).get("Subject"), jsnp.get("name[1]"));
 
         } catch (Exception e) {
             System.out.println("Exception: " + e);
@@ -103,18 +107,18 @@ public class API_Triggering {
     }
 
     @When("I want the to create new {word}")
-    public void iWantTheToCreateNewUsers(String endpoint,DataTable table) {
+    public void iWantTheToCreateNewUsers(String endpoint, DataTable table) {
         try {
-            Request=RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON);
+            Request = RestAssured.given().contentType(ContentType.JSON).accept(ContentType.JSON);
 
-            data=table.asMaps();
+            data = table.asMaps();
             JSONObject reqBody = new JSONObject();
-            reqBody=createPostRequest(endpoint,data);
-            System.out.println("Request: "+reqBody);
-            Response=Request.body(reqBody.toJSONString()).post(endpoint);
+            reqBody = createPostRequest(endpoint, data);
+            System.out.println("Request: " + reqBody);
+            Response = Request.body(reqBody.toJSONString()).post(endpoint);
 
-        }catch (Exception e){
-            System.out.println("Exception: "+e);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
             Assert.assertTrue(false);
         }
     }
@@ -122,12 +126,25 @@ public class API_Triggering {
     @Then("Validate the response code {int}")
     public void validateTheResponseCode(int Response_code) {
         try {
-            Status_code=Response.getStatusCode();
-            Assert.assertEquals("Post Response code validation failed",Response_code,Status_code);
-            System.out.println("Response: "+Response.getBody().asString());
-        }catch (Exception e){
-            System.out.println("Exception: "+e);
+            Status_code = Response.getStatusCode();
+            Assert.assertEquals("Post Response code validation failed", Response_code, Status_code);
+            System.out.println("Response: " + Response.getBody().asString());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
             Assert.assertTrue(false);
         }
     }
+
+    @And("Validate the response with JsonSchema of {word}")
+    public void validateTheResponseWithJsonSchemaOf(String apiName) {
+        try {
+            File file = new File(System.getProperty("user.dir") + "\\JsonSchemas\\" + apiName + ".json");
+            Response.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(file));
+            System.out.println("Schema Validation Successful");
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+            Assert.assertTrue(false);
+        }
+    }
+
 }
